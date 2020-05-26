a177da3fd763b6338e3714f072e535e6bef8ffb2
Cole Wilkison <wilkison.cole@gmail.com>
2020-05-26 11:44:39 -0400

    third commit

--- knexfile.js
+++ knexfile.js
@@ -58,7 +58,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: pgConnection,
     useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations",
