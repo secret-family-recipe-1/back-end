9f5cf1d52a5031a9ee3de1fa321716b4942216eb
Cole Wilkison <wilkison.cole@gmail.com>
2020-05-26 12:10:13 -0400

    deploy 4

--- knexfile.js
+++ knexfile.js
@@ -43,31 +43,20 @@ module.exports = {
   },
 
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
   production: {
-    client: "pg",
-    connection: pgConnection,
-    useNullAsDefault: true,
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+      tableName: 'knex_migrations'
+    }
   },
+
 };
