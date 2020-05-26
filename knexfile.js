08a12ddf51837e7ae3c497e96264a3cfffe81e1c
Cole Wilkison <wilkison.cole@gmail.com>
2020-05-26 11:21:37 -0400

    second commit

--- knexfile.js
+++ knexfile.js
@@ -54,6 +54,17 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
-  }
+  },
 
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };
