4e2dac6b01b3ea38682e0e9e93f4d5e4c469996f
Cole Wilkison <wilkison.cole@gmail.com>
2020-05-27 17:12:37 -0400

    wednesday commit

--- recipes/recipesRouter.spec.js
+++ recipes/recipesRouter.spec.js
@@ -1,106 +1,129 @@
-// // const supertest = require("supertest");
-// // const server = require("../api/server.js");
-// // const db = require("../data/dbconfig.js");
+// const supertest = require("supertest");
+// const server = require("../api/server.js");
+// const db = require("../data/dbconfig.js");
 
 
       
-// //       /// WORKS WELL//
+//       /// WORKS WELL//
 
-// //     describe('GET recipes by Id ', () => {
-// //         it('returns recipe', () => {
-// //           return supertest(server)
-// //             .get('/api/recipes/1')
-// //             .then(res => {
-// //               expect(res.status).toBe(200);
-// //             });
-// //         });
-// //         it('returns a 400 if the recipe is not found', () => {
-// //           return supertest(server)
-// //             .get('/api/recipes/666')
-// //             .then(res => {
-// //               expect(res.status).toBe(400);
-// //             });
-// //         });
-// //       });
+//     describe('GET recipes by Id ', () => {
+//         it('returns recipe', () => {
+//           return supertest(server)
+//             .get('/api/recipes/1')
+//             .then(res => {
+//               expect(res.status).toBe(200);
+//             });
+//         });
+//         it('returns a 400 if the recipe is not found', () => {
+//           return supertest(server)
+//             .get('/api/recipes/666')
+//             .then(res => {
+//               expect(res.status).toBe(400);
+//             });
+//         });
+//       });
 
-// //       describe('GET recipes by Category', () => {
-// //         it('returns category recipe', () => {
-// //           return supertest(server)
-// //             .get('/api/recipes/italian')
-// //             .then(res => {
-// //               expect(res.status).toBe(200);
-// //             });
-// //         });
-// //         it('returns a 400 if the recipe is not found', () => {
-// //           return supertest(server)
-// //             .get('/api/recipes/ethiopian')
-// //             .then(res => {
-// //               expect(res.status).toBe(400);
-// //             });
-// //         });
-// //       });
+//       describe('GET recipes by Category', () => {
+//         it('returns category recipe', () => {
+//           return supertest(server)
+//             .get('/api/recipes/italian')
+//             .then(res => {
+//               expect(res.status).toBe(200);
+//             });
+//         });
+//         it('returns a 400 if the recipe is not found', () => {
+//           return supertest(server)
+//             .get('/api/recipes/ethiopian')
+//             .then(res => {
+//               expect(res.status).toBe(400);
+//             });
+//         });
+//       });
     
 
-// const supertest = require("supertest");
-// const server = require("../api/server");
-// const db = require("../data/dbConfig");
+const supertest = require("supertest");
+const server = require("../api/server");
+const db = require("../data/dbConfig");
 
-// beforeEach(async () => {
-//     await db("recipes").truncate();
-//     await db("users").truncate();
-//   });
+beforeEach(async () => {
+    await db("recipes").truncate();
+    await db("users").truncate();
+  });
 
 
-// const recipe = {
-//     title: "Cake",
-//     source: "Monica",
-//     instructions: "Mix ingredients together, place in over for 45 minutes at 450 degrees",
-//     ingredients: "milk, eggs, flour",
-//     category: "dessert",
-//     img_url: null,
-//     user_id: 1
-//   }
+const recipe = {
+    title: "Cake",
+    source: "Monica",
+    instructions: "Mix ingredients together, place in over for 45 minutes at 450 degrees",
+    ingredients: "milk, eggs, flour",
+    category: "dessert",
+    img_url: null,
+    user_id: 1
+  }
 
-// const recipe2 = {
-//     title: "Pizza",
-//     source: "Thomas",
-//     instructions: "Knead flour into circle, spread tomato sauce, cheese and pepperoni, place in oven",
-//     ingredients: "cheese, tomato sauce flour, pepperoni",
-//     category: "italian",
-//     img_url: null,
-//     user_id: 2
-//   }
+const recipe2 = {
+    title: "Pizza",
+    source: "Thomas",
+    instructions: "Knead flour into circle, spread tomato sauce, cheese and pepperoni, place in oven",
+    ingredients: "cheese, tomato sauce flour, pepperoni",
+    category: "italian",
+    img_url: null,
+    user_id: 2
+  }
 
-// const id = 1;
+const id = 1;
 
-// describe("GET /api/recipes", () => {
-//   it("returns 400 code: no token", async () => {
-//     await supertest(server).get("/api/recipes").expect(400);
-//   });
-//   it("returns 200 code and token", async () => {
-//     await supertest(server)
-//       .post("/api/auth/register")
-//       .send({ username: "test", password: "recipes", location: "BR", name: "Lou" })
-//       .then(async () => {
-//         await supertest(server)
-//           .post("/api/auth/login")
-//           .send({ username: "test", password: "recipes" })
-//           .then(async (res) => {
-//             const token = res.body.token;
-//             await supertest(server)
-//               .get("/api/recipes")
-//               .set("Authorization", token)
-//               .expect(200)
-//               .expect(Array);
-//           });
-//       });
-//   });
-// });
+describe("GET /api/recipes", () => {
+  it("returns 400 code: no token", async () => {
+    await supertest(server).get("/api/recipes").expect(400);
+  });
+  it("returns 200 code and token", async () => {
+    await supertest(server)
+      .post("/api/auth/register")
+      .send({ username: "test", password: "recipes", location: "BR", name: "Lou" })
+      .then(async () => {
+        await supertest(server)
+          .post("/api/auth/login")
+          .send({ username: "test", password: "recipes" })
+          .then(async (res) => {
+            const token = res.body.token;
+            await supertest(server)
+              .get("/api/recipes")
+              .set("Authorization", token)
+              .expect(200)
+              .expect(Array);
+          });
+      });
+  });
+});
 
-// describe("POST /api/recipes", () => {
-//     it("returns 400 code: no token", async () => {
-//         await supertest(server).get("/api/recipes").expect(400);
-//     });
+describe("POST /api/recipes", () => {
+    it("returns 400 code: no token", async () => {
+        await supertest(server).get("/api/recipes").expect(400);
+    });
+    it("returns 200 code and token", async () => {
+        await supertest(server)
+        .post("/api/auth/register")
+        .send({ username: "test", password: "recipes", location: "BR", name: "Lou" })
+        .then(async () => {
+            await supertest(server)
+                .post("/api/auth/login")
+                .send({ username: "test", password: "recipes" })
+                .then(async (res) => {
+                    const token = res.body.token;
+                    await supertest(server)
+                    .get("/api/recipes")
+                    .set("Authorization", token)
+                    .send(recipe)
+                    .expect(200);
+                })
+
+        })
+    })
+
+})
+
+// describe("DELETE /api/recipe/:id", () => {
 //     it("returns 200 code and token", async () => {
 //         await supertest(server)
 //         .post("/api/auth/register")
@@ -111,10 +134,10 @@
 //                 .send({ username: "test", password: "recipes" })
 //                 .then(async (res) => {
 //                     const token = res.body.token;
+//                     await db("recipes").send(recipe);
 //                     await supertest(server)
-//                     .get("/api/recipes")
+//                     .delete(recipe)
 //                     .set("Authorization", token)
-//                     .send(recipe)
 //                     .expect(200);
 //                 })
 
--- users/usersRouter.spec.js
+++ users/usersRouter.spec.js
@@ -13,3 +13,55 @@
 //         expect(res.status).toBe(200);
 //       });
 //     });
+
+const supertest = require("supertest");
+const server = require("../api/server");
+const db = require("../data/dbConfig");
+
+beforeEach(async () => {
+    await db("users").truncate();
+  });
+
+  const user1 = {
+    username: "cole",
+    location: "NY",
+    name: "Nicole"
+  }
+
+  const user2 = {
+    username: "tim",
+    location: "CA",
+    name: "Timothy"
+  }
+
+  const user3 = {
+    username: "vicky",
+    location: "SC",
+    name: "Victoria"
+  }
+
+
+
+describe("GET /api/users", () => {
+    it("returns 400 code: no token", async () => {
+      await supertest(server).get("/api/recipes").expect(400);
+    });
+    it("returns 200 code and token", async () => {
+      await supertest(server)
+        .post("/api/auth/register")
+        .send({ username: "test", password: "recipes", location: "BR", name: "Lou" })
+        .then(async () => {
+          await supertest(server)
+            .post("/api/auth/login")
+            .send({ username: "test", password: "recipes" })
+            .then(async (res) => {
+              const token = res.body.token;
+              await supertest(server)
+                .get("/api/users")
+                .set("Authorization", token)
+                .expect(200)
+                .expect(Array);
+            });
+        });
+    });
+  });
